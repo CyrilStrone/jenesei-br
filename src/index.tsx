bb48a1e59dfc771d3ba70e280a2f2634ec86ade5
Cyril Strone LC <cyrilstrone@gmail.com>
2023-04-06 01:28:47 +0700

    Fonts npm

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,9 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
+import "@fontsource/inter";
+import "@fontsource/montserrat";
+import "@fontsource/roboto";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
