df5350127619731c274c6a4e80b5c8f0992f201e
Cyril Strone <kidvos@gmail.com>
2023-04-17 21:15:11 +0700

    Readme

--- src/common/AxiosInstance.tsx
+++ src/common/AxiosInstance.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 export const accessTokenName = "accessTokenName"
 
 export const axiosInstance = axios.create({
-    baseURL: "http://5.63.154.65:3000/",
+    baseURL: "https://businessroulette.ru:3000/",
     headers: {
 		authorization: `Bearer ${localStorage.getItem(accessTokenName)}`
 	},
