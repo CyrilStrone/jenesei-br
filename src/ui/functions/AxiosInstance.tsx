3bb68d995d09a31a1d53ad241e4646e2e07b3399
Cyril Strone <kidvos@gmail.com>
2023-04-26 00:12:01 +0700

    index

--- src/ui/functions/AxiosInstance.tsx
+++ src/ui/functions/AxiosInstance.tsx
@@ -2,16 +2,16 @@ import axios from "axios";
 
 export const accessTokenName = "accessTokenName"
 
-// export const axiosInstance = axios.create({
-//   baseURL: "https://businessroulette.ru:3000/api/",
-//   headers: {
-//     authorization: `Bearer ${localStorage.getItem(accessTokenName)}`
-//   },
-// });
-
 export const axiosInstance = axios.create({
-  baseURL: "http://26.74.162.51:3002/api/",
+  baseURL: "https://businessroulette.ru:3000/api/",
   headers: {
     authorization: `Bearer ${localStorage.getItem(accessTokenName)}`
   },
 });
+
+// export const axiosInstance = axios.create({
+//   baseURL: "http://26.74.162.51:3002/api/",
+//   headers: {
+//     authorization: `Bearer ${localStorage.getItem(accessTokenName)}`
+//   },
+// });
