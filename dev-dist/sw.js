63dbff01190ea568813c3cf2d0707f97220c6ee7
Cyril Strone <cyrilstrone@gmail.com>
2023-07-31 14:22:06 +0700

    Chat adaptive 2

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.75s2674o7a"
+    "revision": "0.p8rfpf5g1c8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
